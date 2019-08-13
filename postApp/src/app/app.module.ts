40c90cd976f6a07f8c6b1a01e8f5c8ffe2ee67a9
Rutro187 <mjohnson@esomaha.com>
2019-08-13 13:33:27 -0500

    das

---
+++ postApp/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { PostComponent } from './post/post.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    UserComponent,
+    PostComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
